e21360d5a57c73b95fcdcd25a18eb71bf6021c32
SURYADEEPTA <121755483+suryadeepta@users.noreply.github.com>
2023-11-05 10:17:03 +0530

    Add files via upload

--- CIRAL6 Final Edition/CIRAL.py
+++ CIRAL6 Final Edition/CIRAL.py
@@ -20,7 +20,65 @@ from translate import Translator
 from gtts import gTTS
 import smtplib
 import shutil
-import math
+import pyperclip
+import pyautogui
+import webbrowser
+from time import sleep
+import json
+import keyboard
+from bardapi import BardCookies
+
+
+def CookieScrapper():
+    webbrowser.open("https://bard.google.com")
+    sleep(2)
+    pyautogui.click(x=1723, y=64)
+    sleep(1)
+    pyautogui.click(x=1555, y=242)
+    sleep(1)
+    pyautogui.click(x=1443, y=104)
+    sleep(1)
+    keyboard.press_and_release("ctrl + w")
+
+    data = pyperclip.paste()
+
+    try:
+        json_data = json.loads(data)
+        pass
+
+    except json.JSONDecodeError as e:
+        print(f"Error parsing JSON data: {e}")
+
+    SID = "__Secure-1PSID"
+    TS = "__Secure-1PSIDTS"
+    CC = "__Secure-1PSIDCC"
+
+    SIDValue = next((item for item in json_data if item["name"] == SID), None)
+    TSValue = next((item for item in json_data if item["name"] == TS), None)
+    CCValue = next((item for item in json_data if item["name"] == CC), None)
+
+    if SIDValue is not None:
+        SIDValue = SIDValue["value"]
+    else:
+        print(f"{SIDValue} not found in the JSON data.")
+
+    if TSValue is not None:
+        TSValue = TSValue["value"]
+    else:
+        print(f"{TSValue} not found in the JSON data.")
+
+    if CCValue is not None:
+        CCValue = CCValue["value"]
+    else:
+        print(f"{CCValue} not found in the JSON data.")
+
+    cookie_dict = {
+        "__Secure-1PSID": SIDValue,
+        "__Secure-1PSIDTS": TSValue,
+        "__Secure-1PSIDCC": CCValue,
+    }
+
+    return cookie_dict
 
 
 directory_name = "Conversations"
@@ -31,17 +89,6 @@ text_file = open(file_path, "a")
 text_file.write("\n\n\t\tConversations:\n\n")
 text_file.close()
 
-
-# RG er Bucketlist
-
-# random file er kotha-barta
-reply_list = (
-    "Did you said",
-    "ok boss, you said",
-    "understood boss, you said",
-    "you said",
-)
-reply = random.choice(reply_list)
 
 reply_list2 = (
     "Sorry Boss, Can't process. please repeat...",
@@ -131,7 +178,6 @@ def speak(audio):
     engine.runAndWait()
     speaking = False
 
-    # Append the spoken text to the text file
     current_time = time.strftime("%Y-%m-%d %H:%M:%S")
     directory_name = "Conversations"
     if not os.path.exists(directory_name):
@@ -155,12 +201,11 @@ def wishMe():
     else:
         speak("Good Evening!")
 
-    # print("Hello Boss, I am CIRAL, Please tell me how may I help you")
     speak("Hello Boss, I am kairal, Please tell me how may I help you")
 
 
 def set_volume(query):
-    global volume  # Declare 'volume' as a global variable
+    global volume
     if "set volume to" in query:
         new_volume = query.replace("set volume to", "").strip()
         try:
@@ -175,25 +220,18 @@ def get_news():
     news_url = "https://news.google.com/rss?hl=en-US&gl=US&ceid=US:en"
     feed = feedparser.parse(news_url)
     if "entries" in feed:
-        headlines = feed.entries[:5]  # Get the top 5 headlines
-        # print("Here are the top 5 news headlines:")
+        headlines = feed.entries[:5]
         speak("Here are the top 5 news headlines:")
         for index, headline in enumerate(headlines):
-            # print(f"{index + 1}. {headline.title}")
             speak(f"{index + 1}. {headline.title}")
     else:
-        # print(
-        #     "Extreamly Sorry boss, I couldn't fetch the news headlines at the moment."
-        # )
         speak(
             "Extreamly Sorry boss, I couldn't fetch the news headlines at the moment."
         )
 
 
-# Function to get the current weather
 def get_weather(city):
     try:
-        # Create the wttr.in URL for the specified city
         speak("BUt boss, of which city you want to check the weather?")
         try:
             city = takecommand().lower()
@@ -202,73 +240,51 @@ def get_weather(city):
 
         wttr_url = f"https://wttr.in/{city.replace(' ', '+')}?format=%C with a temparature of %t"
 
-        # Send a GET request to wttr.in and parse the response
         response = requests.get(wttr_url)
         if response.status_code == 200:
             soup = BeautifulSoup(response.text, "html.parser")
             weather_info = soup.get_text().strip()
 
-            # Speak the weather information
-            # print(f"The weather in {city} is {weather_info}.")
             speak(f"The weather in {city} is {weather_info}.")
         else:
-            # print("Sorry, I couldn't fetch the weather information at the moment.")
             speak("Sorry, I couldn't fetch the weather information at the moment.")
 
     except Exception as e:
-        # print(f"An error occurred: {e}")
         speak(f"An error occurred: {e}")
-
-
-# Function to solve a math expression
 
 
 def solve_math_expression(expression):
     try:
         result = eval(expression)
-        # print(f"The result of {expression} is {result}")
         speak(f"The result of {expression} is {result}")
     except Exception as e:
-        # print(f"Sorry, I couldn't solve the math expression due to an error: {e}")
         speak(f"Sorry, I couldn't solve the math expression due to an error: {e}")
 
 
 def search_on_youtube(query):
-    # Check if the query contains "find on YouTube"
     if "find on youtube" in query:
-        # Extract the search query (remove "find on YouTube")
         ytsearch_query = query.replace("find on youtube", "").strip()
 
-        # Perform the YouTube search
         search_url = f"https://www.youtube.com/results?search_query={ytsearch_query}"
         webbrowser.open(search_url)
-        # print(f"Searching YouTube for '{ytsearch_query}'...")
         speak(f"Searching YouTube for '{ytsearch_query}'...")
     else:
-        # print("No 'find on YouTube' keyword found in the query.")
         speak("No 'find on YouTube' keyword found in the query.")
 
 
 def open_website(query):
     try:
-        # Check if the query contains "please open"
         if "please open" in query:
-            # Extract the website URL (remove "please open")
             website_name = query.replace("please open", "").strip()
 
-            # Ensure the URL starts with "http://" or "https://"
             if not website_name.startswith("https://"):
                 website_url = "www." + website_name + ".com"
 
-            # Open the website in the default web browser
             webbrowser.open(website_url)
-            # print(f"Opening {website_name} in the web browser...")
             speak(f"Opening {website_name} in the web browser...")
         else:
-            # print("No 'please open' keyword found in the query.")
             speak("No 'please open' keyword found in the query.")
     except Exception as e:
-        # print(f"An error occurred: {e}")
         speak(f"An error occurred: {e}")
 
 
@@ -276,11 +292,11 @@ def send_email(receiver_email, subject, message):
     try:
         server = smtplib.SMTP("smtp.gmail.com", 587)
         server.starttls()
-        server.login("YOUR_EMAIL@gmail.com", "YOUR_PASSWORD")
+        server.login("simasarkarfkt@gmail.com", "uraqqfqbdglccahh")
 
         email_message = f"Subject: {subject}\n\n{message}"
 
-        server.sendmail("YOUR_EMAIL@gmail.com", receiver_email, email_message)
+        server.sendmail("simasarkarfkt@gmail.com", receiver_email, email_message)
         server.quit()
         speak("The email is successfully sent boss")
     except Exception as e:
@@ -293,51 +309,39 @@ def update_image(label, image_list, index):
         label.configure(image=img)
         index += 1
 
-        # Adjust frame delay while speaking
-        delay = 40  # Default frame delay
+        delay = 40
         if speaking:
-            delay = 5  # Reduced frame delay during speech
+            delay = 5
 
         label.after(delay, update_image, label, image_list, index)
     else:
-        # Reset to the first frame when all frames have been displayed
         label.after(0, update_image, label, image_list, 0)
 
 
 def translate_and_speak():
-    # Initialize the speech recognizer
     recognizer = sr.Recognizer()
 
-    # Prompt the user to speak
     speak("Please speak the text you want to translate.")
 
     with sr.Microphone() as source:
-        # Adjust for ambient noise and listen to user input
         recognizer.adjust_for_ambient_noise(source)
         audio = recognizer.listen(source)
 
     try:
-        # Recognize the speech
         user_input = recognizer.recognize_google(audio)
-        # print(f"You said: {user_input}")
 
-        # Prompt the user to choose the target language
         speak("Say the target language.")
         target_language_input = takecommand().lower()
 
-        # Check if the target language is in the language_codes dictionary
         if target_language_input in language_codes:
             target_language = language_codes[target_language_input]
         else:
             speak("No language found. Please try again.")
             return
-        # Initialize the translator
         translator = Translator(to_lang=target_language)
-        # Translate the user's input to the chosen language
         translation = translator.translate(user_input)
         translated_text = translation
 
-        # Save the translation to a log file
         directory_name = "Conversations"
         if not os.path.exists(directory_name):
             os.makedirs(directory_name)
@@ -345,9 +349,8 @@ def translate_and_speak():
         with open(file_path, "a", encoding="utf-8") as text_file:
             text_file.write(f"Translation to {target_language}: {translated_text}\n")
 
-        # Generate and save the audio for the translated text
         if target_language == "bn":
-            language = target_language  # Use the Indian English accent for Bengali
+            language = target_language
         else:
             language = "en"
 
@@ -366,7 +369,6 @@ def translate_and_speak():
         time.sleep(1)
         os.startfile(file_path)
 
-        # Calculate speaking time and wait for it to finish
         words = translated_text.split()
         num_words = len(words)
         speaking_rate_wpm = 150
@@ -374,7 +376,6 @@ def translate_and_speak():
         time.sleep(speaking_time_minutes * 60 + 1)
 
     except Exception as e:
-        # print(e)
         speak("An error occurred.")
 
 
@@ -382,13 +383,11 @@ def takecommand():
     r = sr.Recognizer()
     with sr.Microphone() as source:
         r.adjust_for_ambient_noise(source, duration=1)
-        # print("Listening...")
         speak("Listening...")
         r.pause_threshold = 1
         audio = r.listen(source)
 
     try:
-        # print("Recognizing...")
         speak("Recognizing...")
         query = r.recognize_google(audio, language="en-in")
 
@@ -400,12 +399,7 @@ def takecommand():
         with open(file_path, "a") as text_file:
             text_file.write(f"{current_time} - Boss: {query}\n")
 
-        # print(f"{reply}:'{query}'\n")
-        # speak(f"{reply}: {query}")
-
     except Exception as e:
-        # #print("Say that again, please...")
-        # print(reply2)
         speak(reply2)
         return "None"
     return query
@@ -421,12 +415,10 @@ def check():
         speak("Hola boss, please give me some instructions")
 
     elif "wikipedia" in query:
-        # print("Searching in Wikipedia...")
         speak("Searching in Wikipedia...")
         query = query.replace("wikipedia", "")
         try:
             results = wikipedia.summary(query, sentences=3)
-            # print("According to Wikipedia")
             speak("According to Wikipedia")
             directory_name = "Conversations"
             if not os.path.exists(directory_name):
@@ -436,11 +428,9 @@ def check():
                 current_time = time.strftime("%Y-%m-%d %H:%M:%S")
                 text_file.write(f"{current_time} - Wikipedia results: {results}\n")
 
-            # print(results)
             speak(results.encode("utf-8"))
             text_file.close()
         except Exception as e:
-            # print("sorry boss, no search results found")
             speak("sorry boss, no search results found, try again")
             check()
 
@@ -448,19 +438,16 @@ def check():
         set_volume(query)
         return query
     elif "email" in query:
-        # print("whom to send the email?")
         speak("whom to send the email?")
         receiver_email = (
             input("Enter the email address of the recipient: ") + "@gmail.com"
         )
-        # print("What will be the subject boss?")
         speak("What will be the subject boss?")
         r = sr.Recognizer()
         with sr.Microphone() as source:
             audio = r.listen(source)
 
         subject = r.recognize_google(audio, language="en")
-        # print("What do you have to send? , please tell me the message")
         speak("What do you have to send? , please tell me the message")
         r = sr.Recognizer()
         with sr.Microphone() as source:
@@ -476,7 +463,6 @@ def check():
 
     elif "time" in query:
         strTime = datetime.datetime.now().strftime("%H:%M:%S")
-        # print(f"Sir, the time is {strTime}")
         speak(f"Sir, the time is {strTime}")
 
     elif "search" in query:
@@ -484,8 +470,6 @@ def check():
 
         webbrowser.open(f"https://www.google.com/search?q={search_query}")
         speak(f"Searching Google for {search_query}")
-
-    # youtube video player.
 
     elif "find on youtube" in query:
         search_on_youtube(query)
@@ -514,20 +498,15 @@ def check():
         pyautogui.click(x=1482, y=1066)
         speak("bluetooth has been disconnected")
         time.sleep(1)
-    # general kothobokothon😝
 
     elif "how are you" in query:
         speak("I am fine ,and always at your service. Boss")
-
-    # news o bole re baba
 
     elif "news" in query:
         get_news()
 
     elif "weather" in query:
-        get_weather("city")
-
-    # math e o valo
+        get_weather("falakata")
 
     elif "solve" in query:
         expression = query.replace("solve", "").strip()
@@ -538,8 +517,6 @@ def check():
         result = eval(q)
         speak(f"The result is {result}")
 
-    # Music Chalate Pare
-
     elif "roll the music" in query:
         pyautogui.hotkey("win", "s")
         time.sleep(2)
@@ -547,11 +524,9 @@ def check():
         time.sleep(2)
         pyautogui.press("enter")
 
-        # Wait for Spotify to open
         time.sleep(3)
 
-        # Search for a song
-        pyautogui.hotkey("ctrl", "k")  # Focus on the search bar
+        pyautogui.hotkey("ctrl", "k")
         time.sleep(2)
         pyautogui.write(query.replace("roll the music", "").strip())
         time.sleep(2)
@@ -579,34 +554,32 @@ def check():
         pyautogui.hotkey("alt", "fn", "f4")
         time.sleep(1)
 
+    elif "show me the conversations" in query:
+        directory_name = "Conversations"
+        file_path = os.path.join(directory_name, "conversation_log.txt")
+        text_file = open(file_path, "a")
+
+
     elif "clear conversation history" in query:
         if os.path.exists("Conversations"):
             shutil.rmtree("Conversations")
 
-            # print("Previous conversation history has been deleted.")
             speak("Previous conversation history has been deleted.")
 
         else:
-            # print("conversation history does not exist.")
             speak("conversation history does not exist.")
 
     elif "delete all the audio files" in query:
         if os.path.exists("Audios"):
             shutil.rmtree("Audios")
 
-            # print("All audio files has been deleted.")
             speak("All audio files has been deleted.")
 
         else:
-            # print("No audio file is there in the folder boss")
             speak("No audio file is there in the folder boss")
 
-    # Biday bela
-
     elif "turn off yourself" in query:
-        # print("Stop command received, exiting script.")
-        speak("ok boss, BY.")  # see you in a while
-        # speak("Stop command received, exiting script.")
+        speak("ok boss, BY.")
         directory_name = "Conversations"
         if not os.path.exists(directory_name):
             os.makedirs(directory_name)
@@ -621,9 +594,7 @@ def check():
             exit()
 
     elif "shutdown" in query:
-        # print("Turning of the PC.")
         speak("Turning of the PC.")
-        # os.system( "shutdown -s")
         directory_name = "Conversations"
         if not os.path.exists(directory_name):
             os.makedirs(directory_name)
@@ -636,7 +607,12 @@ def check():
         else:
             os.system("shutdown /s /t 0")
     else:
-        speak("Sorry boss, no valid query found")
+        speak("Please give me few seconds boss")
+        # speak(f"Boss, Give me few seconds to porcess about {query}")
+        cookie_dict = CookieScrapper()
+        bard = BardCookies(cookie_dict=cookie_dict)
+        Reply = bard.get_answer(query)["content"]
+        speak(Reply)
 
 
 def sudo_Analyze():
@@ -658,15 +634,14 @@ def main():
 
 
 def wake_word():
-    hour = int(datetime.datetime.now().hour)
-    if hour >= 3 and hour < 11:
-        sudo_Analyze()
-    query = takecommand()
-    if "hello" in query:
-        main()
-    elif "no" in query:
-        speak("ok boss, see you in a while")
-        pyautogui.hotkey("alt", "fn", "f4")
+    sudo_Analyze()
+    while True:
+        query = takecommand()
+        if "hello" in query:
+            main()
+        elif "no" in query:
+            speak("ok boss, see you in a while")
+            pyautogui.hotkey("alt", "fn", "f4")
 
 
 root = ctk.CTk()
@@ -675,7 +650,6 @@ root.iconbitmap("Assets\LOGO.ico")
 root.protocol("WM_DELETE_WINDOW", root.destroy)
 
 speaking = False
-# Open the animated GIF and split it into frames
 image = Image.open("Assets\DO5.gif")
 frames = [ImageTk.PhotoImage(frame) for frame in ImageSequence.Iterator(image)]
 
